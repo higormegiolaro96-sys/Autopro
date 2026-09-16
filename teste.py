e8eadd64240515e93d9cedd9cc0bfeaaac2faf8d
higormegiolaro96-sys <higormegiolaro96@gmail.com>
2026-09-15 21:47:24 -0300

    Add pressure sensor check and alerts

--- teste.py
+++ teste.py
@@ -1,1 +1,7 @@
-print("Olá Mundo! Meu primeiro código de Indústria 4.0.")
+
+sensor_pressao = 3.2
+
+if sensor_pressao > 4.0:
+    print("⚠️ ALERTA: Pressão acima do limite! Parar linha de produção.")
+else:
+    print("✅ STATUS: Pressão normal. Linha operando.")
